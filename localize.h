bbce4fbd080f154ad4ca1885478401ba23e8a4bf
= <github@nicholasjohnson.info>
2025-01-11 01:02:45 -0500

    Added serializable structs for zone file, localize asset, and rawfile asset.

---
+++ localize.h
@@ -0,0 +1,55 @@
+#ifndef LOCALIZE_H
+#define LOCALIZE_H
+
+#include <QDataStream>
+#include <QString>
+
+struct Localize {
+    quint32 valuePtr;
+    quint32 namePtr;
+    QString referenceStr;
+    QString localizedStr;
+};
+
+QDataStream &operator<<(QDataStream &out, const Localize &localize) {
+    out << localize.valuePtr << localize.namePtr;
+
+    // Parse gsc contents
+    QByteArray localizedStrData(localize.localizedStr.toUtf8());
+    out.writeRawData(localizedStrData.data(), localizedStrData.size());
+
+    // Parse rawfile path
+    QByteArray refStrData(localize.referenceStr.toUtf8());
+    out.writeRawData(refStrData.data(), refStrData.size());
+
+    return out;
+}
+
+QDataStream &operator>>(QDataStream &in, Localize &localize) {
+    // Parse value and name pointers
+    in >> localize.valuePtr >> localize.namePtr;
+
+    // Parse localized string
+    QString localizedStr;
+    char localizedStrChar;
+    in >> localizedStrChar;
+    while (localizedStrChar != 0) {
+        localizedStr += localizedStrChar;
+        in >> localizedStrChar;
+    }
+    localize.localizedStr = localizedStr;
+
+    // Parse localized reference name
+    QString refStr;
+    char refStrChar;
+    in >> refStrChar;
+    while (refStrChar != 0) {
+        refStr += refStrChar;
+        in >> refStrChar;
+    }
+    localize.referenceStr = refStr;
+
+    return in;
+}
+
+#endif // LOCALIZE_H
---
+++ rawfile.h
@@ -0,0 +1,61 @@
+#ifndef RAWFILE_H
+#define RAWFILE_H
+
+#include <qtcoreexports.h>
+#include <QDataStream>
+#include <QString>
+
+struct RawFile {
+    quint32 gscNamePtr;
+    quint32 gscLength;
+    quint32 rawFilePathPtr;
+    QString rawFilePath;
+    QString rawFileContents;
+};
+
+QDataStream &operator<<(QDataStream &out, const RawFile &rawFile) {
+    out << rawFile.gscNamePtr;
+    out << rawFile.gscLength;
+    out << rawFile.rawFilePathPtr;
+
+    // Parse rawfile path
+    QByteArray rawFilePathData(rawFile.rawFilePath.toUtf8());
+    out.writeRawData(rawFilePathData.data(), rawFilePathData.size());
+
+    // Parse gsc contents
+    QByteArray rawFileContentsData(rawFile.rawFileContents.toUtf8());
+    out.writeRawData(rawFileContentsData.data(), rawFileContentsData.size());
+
+    return out;
+}
+
+QDataStream &operator>>(QDataStream &in, RawFile &rawFile) {
+    in >> rawFile.gscNamePtr;
+    in >> rawFile.gscLength;
+    in >> rawFile.rawFilePathPtr;
+
+    // Parse rawfile path
+    QString rawFilePath;
+    char scriptPathChar;
+    in >> scriptPathChar;
+    while (scriptPathChar != 0) {
+        rawFilePath += scriptPathChar;
+        in >> scriptPathChar;
+    }
+    rawFile.rawFilePath = rawFilePath;
+
+    // Parse gsc contents
+    QString rawFileContents;
+    char rawFileContentsChar;
+    in >> rawFileContentsChar;
+    while (rawFileContentsChar != 0 && rawFileContentsChar != -1) {
+        rawFileContents += rawFileContentsChar;
+        in >> rawFileContentsChar;
+    }
+    rawFile.rawFileContents = rawFileContents;
+
+    return in;
+}
+
+
+#endif // RAWFILE_H
